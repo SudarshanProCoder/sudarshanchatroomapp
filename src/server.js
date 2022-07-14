1dcfda2cb44838e7d57b5de79972233baad9ceb3
SudarshanProCoder <sudarshandate21@gmail.com>
2022-07-14 22:37:37 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const hostname = '192.168.29.49';
+const port = 5000;
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+app.use(express.static(path.join(__dirname + "/public")));
+io.on("connection", function(socket) {
+    socket.on("newuser", function(username) {
+        socket.broadcast.emit("update", username + " joined the conversation");
+    });
+    socket.on("exituser", function(username) {
+        socket.broadcast.emit("update", username + " left the conversation");
+    });
+    socket.on("chat", function(message) {
+        socket.broadcast.emit("chat", message);
+    });
+});
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}/`)
+});
